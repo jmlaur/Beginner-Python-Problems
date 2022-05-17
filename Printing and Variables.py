032bbef0edc72971e89384c753585c67d31f0f6f
jmlaur <103467834+jmlaur@users.noreply.github.com>
2022-05-17 15:42:02 -0400

    Printing and Variables

---
+++ Printing and Variables.py
@@ -0,0 +1,43 @@
+#1
+
+print("Hello World")
+
+#2
+
+msg1 = "Message1"
+print(msg1)
+
+#3
+
+msg = "Message"
+print(msg)
+msg = "New Message"
+print(msg)
+
+#4
+
+age = 17
+name = "James"
+
+print(name + str(age))
+print(name, age)
+
+#5
+
+a = 20
+o = 15
+
+print("Number of Apples: ", a)
+print("Number of Oranges: ", o)
+
+o = 0
+
+print("Number of Apples: ", a)
+print("Number of Oranges: ", o)
+
+#extra
+
+persons_name = "John"
+print(persons_name.upper())
+print(persons_name.lower())
+print(persons_name.title())
