5e76f2d1cdc16019c0485b6ef635aec4e22b254c
jmlaur <103467834+jmlaur@users.noreply.github.com>
2022-05-17 22:49:48 -0400

    Add files via upload

---
+++ User Input and Variables.py
@@ -0,0 +1,40 @@
+#1
+
+print(4*2)
+print(4+4)
+print(9-1)
+print(16/2)
+
+
+#2
+
+num = 4
+
+print("My favorite number is ", num)
+
+#3
+      
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+plnoun1 = input("Plural Noun: ")
+game = input("Game: ")
+plnoun2 = input("Plural Noun: ")
+verb1 = input("Verb ending in 'ING': ")
+verb2 = input("Verb ending in 'ING': ")
+
+print("A vacation is when you take a trip to some ", adj1,
+      " place with your ", adj2,
+      " family. Usually you go to some place that is near a/an ", noun1, " or up on a/an ", noun2,
+      ". A good vacation place is one where you can ride ", plnoun1, " or play ", game,
+      " or go hunting for ", plnoun2, ". I like to spend my time ", verb1, " or ", verb2, ".")
+
+#4
+
+cost = int(input("How much did your meal cost?"))
+tip = cost*0.18
+
+total = cost + tip
+
+print(total)
