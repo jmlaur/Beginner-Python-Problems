82d8cbd8ff0e5f7768d8f8cffaf7988433908a30
jmlaur <103467834+jmlaur@users.noreply.github.com>
2022-05-17 22:46:33 -0400

    Add files via upload

---
+++ Letter Match.py
@@ -0,0 +1,11 @@
+#Letter Match
+
+name1 = input("What is the first name?")
+name2 = input("What is the second name?")
+
+true = ['t', 'r', 'u', 'e']
+
+name1test = name1.count("t")
+name2test = name2.count("t")
+print(name1test)
+print(name2test)
