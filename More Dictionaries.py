12ada4fa2760f2ff58a489e16c51207a18dfb402
jmlaur <103467834+jmlaur@users.noreply.github.com>
2022-05-17 23:40:28 -0400

    Add files via upload

---
+++ More Dictionaries.py
@@ -0,0 +1,49 @@
+
+
+#1
+
+tyler = {
+    "first_name": "tyler",
+    "last_name": "nguyen",
+    "age": "17",
+    "city": "birmingham"
+}
+
+print(tyler["first_name"])
+print(tyler["last_name"])
+print(tyler["age"])
+print(tyler["city"])
+
+
+#2
+
+fav_num = {
+    "bob": 5,
+    "joe": 3,
+    "thomas": 7,
+    "kyle": 45,
+    "avery": 32
+}
+
+print("Bob", fav_num["bob"])
+print("Joe", fav_num["joe"])
+print("Thomas", fav_num["thomas"])
+print("Kyle", fav_num["kyle"])
+print("Avery", fav_num["avery"])
+
+
+#3
+
+glossary = {
+    "list": "A container of items.",
+    "boolean": "True or false.",
+    "break": "To stop this line of code.",
+    "loop": "To continuously do something.",
+    "range": "Area of variation between a set scale."
+}
+
+print("List: \n", glossary["list"])
+print("Boolean: \n", glossary["boolean"])
+print("Break: \n", glossary["break"])
+print("Loop: \n", glossary["loop"])
+print("Range: \n", glossary["range"])
