c972a162f27abbbae34c06bd7f4d5a5aefcedc27
jmlaur <103467834+jmlaur@users.noreply.github.com>
2022-05-17 23:39:43 -0400

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,17 @@
+#1
+
+classes = {
+    1:"Advanced Robotics",
+    2:"Multicultural Food",
+    3:"Programming B",
+    4:"Microeconomics",
+    5:"AP Calculus AB"
+}
+
+print(classes[1])
+print(classes)
+
+#2
+
+classes[6] = "AP Calculus BC"
+print(classes[6])
